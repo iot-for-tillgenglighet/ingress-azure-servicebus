fe6e86bb30e6f8298f5dec4f2093fb407189f657
Håkan Wiklund <hakan.wiklund@gmail.com>
2020-04-09 13:39:18 +0200

    Changed to WebApi and Worker

---
+++ Ingress.Asb.Worker/ServiceBusClient.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Ingress.Asb.Worker
+{
+    public class ServiceBusClient : BackgroundService
+    {
+        private readonly ILogger<ServiceBusClient> _logger;
+
+        public ServiceBusClient(ILogger<ServiceBusClient> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
