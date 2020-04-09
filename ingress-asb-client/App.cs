7255100c45d05f825d4cf3a4517184584f94d286
Håkan Wiklund <hakan.wiklund@gmail.com>
2020-04-09 11:47:13 +0200

    Added DI, logging, and config

---
+++ ingress-asb-client/App.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ingress_asb_client
+{
+    class App
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger _logger;
+
+        public App(IConfiguration config, ILogger<App> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            _logger.LogInformation("logging information");
+
+            Console.WriteLine("Hello from App.cs");
+        }
+
+    }
+}
--- ingress-asb-client/Program.cs
+++ ingress-asb-client/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ingress_asb_client
 {
@@ -6,7 +10,41 @@ namespace ingress_asb_client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+            services.AddLogging(logging =>
+            {
+                logging.AddConfiguration(config.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddFile("Logs/myapp-{Date}.txt");
+            }).Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Information);
+
+            // required to run the application
+            services.AddTransient<App>();
+
+            return services;
+        }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
         }
     }
 }
