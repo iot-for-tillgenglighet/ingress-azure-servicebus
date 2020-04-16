d05f879929a3f64676457d1144d5ea77a3794d75
Håkan Wiklund <hakan.wiklund@gmail.com>
2020-04-16 11:42:32 +0200

    Added messagehandler and model

---
+++ Ingress.Asb.Worker/RoadAvailabilityModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace Ingress.Asb.Worker
+{
+    public partial class RoadAvailabilityModel
+    {
+        [JsonProperty("created")]
+        public DateTimeOffset Created { get; set; }
+
+        [JsonProperty("predictions")]
+        public List<Prediction> Predictions { get; set; }
+
+        [JsonProperty("position")]
+        public Position Position { get; set; }
+    }
+
+    public partial class Position
+    {
+        [JsonProperty("status")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Status { get; set; }
+
+        [JsonProperty("accuracy")]
+        public string Accuracy { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("angle")]
+        public string Angle { get; set; }
+    }
+
+    public partial class Prediction
+    {
+        [JsonProperty("probability")]
+        public double Probability { get; set; }
+
+        [JsonProperty("tagName")]
+        public string TagName { get; set; }
+    }
+
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+
+}
--- Ingress.Asb.Worker/ServiceBusClient.cs
+++ Ingress.Asb.Worker/ServiceBusClient.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -25,13 +25,54 @@ namespace Ingress.Asb.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var myKeyValue = _configuration["Connectionstring"];
-
+            _logger.LogInformation($"Starting servicebus client");
+            RegisterOnMessageHandlerAndReceiveMessages();
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
+            _logger.LogInformation($"Stopping servicebus client");
+        }
+
+        private void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of concurrent calls to the callback ProcessMessagesAsync(), set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether the message pump should automatically complete the messages after returning from user callback.
+                // False below indicates the complete operation is handled by the user callback as in ProcessMessagesAsync().
+                AutoComplete = false
+            };
+
+            // Register the function that processes messages.
+            _subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        private async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message.
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the subscriptionClient is created in ReceiveMode.PeekLock mode (which is the default).
+            await _subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the subscriptionClient has already been closed.
+            // If subscriptionClient has already been closed, you can choose to not call CompleteAsync() or AbandonAsync() etc.
+            // to avoid unnecessary exceptions.
+        }
+
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            _logger.LogInformation($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+
+            // Todo: Error handling. Hur skall vi lägga meddelandet på deadletter kön?
+
+            return Task.CompletedTask;
         }
     }
 }
